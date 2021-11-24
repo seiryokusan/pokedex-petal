e59c451741bedefc83d7975b97be1f22254e807f
Jessica DeBlois <seiryoku.san@gmail.com>
2021-11-23 21:10:54 -0500

    Changed routing to the right component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
-import { PokedexEntryComponent } from "./pokedex-entry/pokedex-entry.component";
+import { PokedexDetailsComponent } from "./pokedex-details/pokedex-details.component";
 import { PokedexMainComponent } from "./pokedex-main/pokedex-main.component";
 
 const routes: Routes = [
-    { path: '', redirectTo: '/pokedex', pathMatch: 'full' },
+    { path: "", redirectTo: "/pokedex", pathMatch: "full" },
     { path: "pokedex", component: PokedexMainComponent },
-    { path: "entry/:id", component: PokedexEntryComponent },
+    { path: "pokedex/entry/:id", component: PokedexDetailsComponent },
 ];
 
 @NgModule({
