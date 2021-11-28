8474de6720d9db31d584900ce0b3e9a5f327eb59
Jessica DeBlois <seiryoku.san@gmail.com>
2021-11-28 12:37:37 -0500

    Details page creation

--- src/app/pokedex-details/pokedex-details.component.ts
+++ src/app/pokedex-details/pokedex-details.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
-import { ActivatedRoute } from "@angular/router";
-import { Pokemon, PokemonClient } from "pokenode-ts";
+import { ActivatedRoute, NavigationEnd, Router } from "@angular/router";
+import { ChainLink, EvolutionChain, Pokemon, PokemonClient, UtilityClient } from "pokenode-ts";
+import { PokemonUtils } from "../../utils/pokemon-utils";
 
 @Component({
     selector: "app-pokedex-details",
@@ -9,18 +10,75 @@ import { Pokemon, PokemonClient } from "pokenode-ts";
 })
 export class PokedexDetailsComponent implements OnInit {
     public selectedPokemon?: Pokemon;
-    private pokemonClient: PokemonClient;
+    public evolutionsPokemon?: Pokemon[];
+    public pokemonClient: PokemonClient;
+    public utilityClient: UtilityClient;
+    public navigationSubscription;
 
-    constructor(private route: ActivatedRoute) {
+    constructor(private router: Router, private route: ActivatedRoute) {
         this.pokemonClient = new PokemonClient();
+        this.utilityClient = new UtilityClient();
+        this.navigationSubscription = this.router.events.subscribe((e: any) => {
+            if (e instanceof NavigationEnd) {
+                this.ngOnInit();
+            }
+        });
     }
 
     ngOnInit(): void {
         this.pokemonClient
             .getPokemonById(Number(this.route.snapshot.paramMap.get("id")))
-            .then((data) => {
-                this.selectedPokemon = data;
+            .then((pokemon) => {
+                this.selectedPokemon = pokemon;
+                this.pokemonClient.getPokemonSpeciesByName(pokemon.species.name).then((species) => {
+                    this.utilityClient.getResourceByUrl(species.evolution_chain.url).then((chain: EvolutionChain) => {
+                        this.getEvolutionChainNames(chain);
+                    });
+                });
             })
             .catch((error) => console.error(error));
+    }
+
+    ngOnDestroy() {
+        if (this.navigationSubscription) {
+            this.navigationSubscription.unsubscribe();
+        }
+        this.selectedPokemon = undefined;
+        this.evolutionsPokemon = undefined;
+    }
+
+    private recursiveEvolvesInto(evolvesInto: ChainLink[], evolutionChainNamesBuffer: string[]) {
+        evolvesInto.forEach((e) => {
+            evolutionChainNamesBuffer.push(e.species.name);
+            if (e.evolves_to.length) {
+                this.recursiveEvolvesInto(e.evolves_to, evolutionChainNamesBuffer);
+            }
+        });
+    }
+
+    private getPokemons(names: string[]) {
+        const groupOfCalls = [];
+        for (const name of names) {
+            if (name === this.selectedPokemon?.name) {
+                groupOfCalls.push(this.selectedPokemon);
+            } else {
+                groupOfCalls.push(this.pokemonClient.getPokemonByName(name));
+            }
+        }
+        return Promise.all(groupOfCalls);
+    }
+
+    private async getEvolutionChainNames(chain: EvolutionChain) {
+        const evolutionChainNamesBuffer: string[] = [chain.chain.species.name || ""];
+        this.recursiveEvolvesInto(chain.chain.evolves_to || [], evolutionChainNamesBuffer);
+        this.evolutionsPokemon = await this.getPokemons(evolutionChainNamesBuffer);
+    }
+
+    get selectedPokemonTypes() {
+        return PokemonUtils.pokemonTypesNames(this.selectedPokemon?.types || []);
+    }
+
+    get pokemonNumberDisplay(): string {
+        return PokemonUtils.pokemonNumberDisplay(this.selectedPokemon?.id || 0);
     }
 }
